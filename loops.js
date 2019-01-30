8b9fa11447e902aed62a4d72e2458756e8b09953
Christine Cua <cloudycua@gmail.com>
2019-01-30 23:52:10 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -26,7 +26,7 @@ function doWhileLoop(array){
 
   do {
       console.log('array.length = ' + array.length + ' and i = ' + i);
-      array.slice(1);
+      array = array.slice(1);
       incrementVariable();
     } while (incrementVariable() < array.length);
     return array;
